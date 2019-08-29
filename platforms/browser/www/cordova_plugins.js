b7ec9e6e58b45f5896a6931650cbc53f29dfdf7e
Mayank_MP5 <mayank5pande@gmail.com>
2019-08-29 23:00:55 +0530

    Admob Ad Not Wroking Properly

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,9 +1,40 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-admobpro/www/AdMob.js",
+        "id": "cordova-plugin-admobpro.AdMob",
+        "pluginId": "cordova-plugin-admobpro",
+        "clobbers": [
+            "window.AdMob"
+        ]
+    },
+    {
+        "file": "plugins/cordova-connectivity-monitor/www/connectivity.js",
+        "id": "cordova-connectivity-monitor.connectivity",
+        "pluginId": "cordova-connectivity-monitor",
+        "clobbers": [
+            "window.connectivity"
+        ]
+    },
+    {
+        "file": "plugins/cordova-admob/www/admob.js",
+        "id": "cordova-admob.AdMobAds",
+        "pluginId": "cordova-admob",
+        "clobbers": [
+            "window.admob",
+            "window.tappx"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-extension": "1.5.4",
+    "cordova-plugin-admobpro": "2.37.2",
+    "cordova-connectivity-monitor": "1.2.2",
+    "cordova-play-services-version-adapter": "1.0.2",
+    "cordova-admob": "5.1.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/google-ads.js
+++ platforms/browser/www/google-ads.js
@@ -1,28 +1,49 @@
-function adsettler(){
-    console.log(navigator.userAgent);
-    var admobid={}
-    if( /(android)/i.test(navigator.userAgent) ) {
-    admobid = { // for Android
+var admobid = {};
+
+// TODO: replace the following ad units with your own
+if( /(android)/i.test(navigator.userAgent) ) {
+  admobid = { // for Android
     banner: 'ca-app-pub-9553154435634450/4946669801',
-    //interstitial: 'ca-app-pub-xxxxxx/xxxxxx'
-    }
-    }
-    else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-    admobid = { // for iOS
+    //interstitial: 'ca-app-pub-3940256099942544/1033173712',
+    //rewardvideo: 'ca-app-pub-3940256099942544/5224354917',
+  };
+} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+  admobid = { // for iOS
     banner: 'ca-app-pub-9553154435634450/4946669801',
-    //interstitial: 'ca-app-pub-xxxxxxx/xxxxxxxx'
-    };
-    }
-    else {
-    admobid = { // for Windows Phone
-    banner: 'ca-app-pub-9553154435634450/4946669801',
-    //interstitial: 'ca-app-pub-xxxxxxx/xxxxxxxx'
-    };
-    }
-    //code for pop-up-ads
-    if(AdMob) AdMob.createBanner({
-    adId:admobid.banner,
-    position:AdMob.AD_POSITION.BOTTOM_CENTER,
-    autoShow:true
-    });
+    //interstitial: 'ca-app-pub-3940256099942544/4411468910',
+    //rewardvideo: 'ca-app-pub-3940256099942544/1712485313',
+  };
+} else {
+  admobid = { // for Windows Phone, deprecated
+    banner: '',
+    interstitial: '',
+    rewardvideo: '',
+  };
+}
+
+function initApp() {
+  if (! AdMob ) { alert( 'admob plugin not ready' ); return; }
+
+  // this will create a banner on startup
+  AdMob.createBanner( {
+    adId: admobid.banner,
+    position: AdMob.AD_POSITION.BOTTOM_CENTER,
+    isTesting: true, // TODO: remove this line when release
+    overlap: false,
+    offsetTopBar: false,
+    bgColor: 'black'
+  } );
+
+  // this will load a full screen ad on startup
+  AdMob.prepareInterstitial({
+    adId: admobid.interstitial,
+    isTesting: true, // TODO: remove this line when release
+    autoShow: true
+  });
+}
+
+if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+    document.addEventListener('deviceready', initApp, false);
+} else {
+    initApp();
 }
